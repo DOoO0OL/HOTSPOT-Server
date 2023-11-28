edd76db094a629aac4b91d0c2d2ec54b6e2d847a
KimGyeongsuuu <sk62628504@gmail.com>
2023-11-28 20:12:02 +0900

    add :: TokenParseAdapter

---
+++ hotspot-api/src/main/kotlin/com/dol/global/security/jwt/TokenParseAdapter.kt
@@ -0,0 +1,49 @@
+package com.dol.global.security.jwt
+
+import com.dol.domain.user.enums.Authority
+import com.dol.global.security.jwt.common.exception.InvalidTokenTypeException
+import com.dol.global.security.jwt.common.properties.JwtProperties
+import com.dol.global.security.principal.AdminDetailsService
+import com.dol.global.security.principal.UserDetailsService
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.security.Key
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class TokenParseAdapter(
+    private val userDetailsService: UserDetailsService,
+    private val adminDetailsService: AdminDetailsService,
+    private val jwtProperties: JwtProperties
+) {
+     fun parseAccessToken(request: HttpServletRequest): String? =
+        request.getHeader(JwtProperties.TOKEN_HEADER)
+            .let { it ?: return null }
+            .let { if (it.startsWith(JwtProperties.TOKEN_PREFIX)) it.replace(JwtProperties.TOKEN_PREFIX, "") else null }
+
+    fun parseRefreshTokenToken(refreshToken: String): String? =
+        if (refreshToken.startsWith(JwtProperties.TOKEN_PREFIX)) refreshToken.replace(JwtProperties.TOKEN_PREFIX, "") else null
+
+    fun authentication(token: String): Authentication =
+        getAuthority(getTokenBody(token, jwtProperties.accessSecret))
+            .let { UsernamePasswordAuthenticationToken(it, "", it.authorities) }
+
+    fun getTokenBody(token: String, secret: Key): Claims =
+        Jwts.parserBuilder()
+            .setSigningKey(secret)
+            .build()
+            .parseClaimsJws(token)
+            .body
+
+    fun getAuthority(body: Claims): UserDetails =
+        when (body.get(JwtProperties.AUTHORITY, String::class.java)) {
+            Authority.ROLE_USER.name -> userDetailsService.loadUserByUsername(body.subject)
+            Authority.ROLE_ADMIN.name -> adminDetailsService.loadUserByUsername(body.subject)
+            else -> throw InvalidTokenTypeException()
+        }
+
+}
