de3fea7576b624745351012b07c0b1b82c97c15e
KimGyeongsuuu <sk62628504@gmail.com>
2023-11-29 23:28:59 +0900

    feat :: 쿼리메서드 구현

--- hotspot-domain/src/main/kotlin/com/dol/domain/user/repository/UserRepository.kt
+++ hotspot-domain/src/main/kotlin/com/dol/domain/user/repository/UserRepository.kt
@@ -4,4 +4,7 @@ import com.dol.domain.user.entity.User
 import org.springframework.data.repository.CrudRepository
 import java.util.UUID
 
-interface UserRepository : CrudRepository<User, UUID>
+interface UserRepository : CrudRepository<User, UUID> {
+    fun existsById(id: String): Boolean
+    fun existsByNickName(nickName: String): Boolean
+}
