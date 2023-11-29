5e4ff1dc96ff8bb63240ff3d9f56f8bb253b342a
KimGyeongsuuu <sk62628504@gmail.com>
2023-11-29 23:27:18 +0900

    feat :: 회원가입 service 구현

---
+++ hotspot-api/src/main/kotlin/com/dol/domain/auth/service/impl/SignUpServiceImpl.kt
@@ -0,0 +1,39 @@
+package com.dol.domain.auth.service.impl
+
+import com.dol.common.util.SecurityUtil
+import com.dol.domain.auth.exception.DuplicateIdException
+import com.dol.domain.auth.exception.DuplicateNickNameException
+import com.dol.domain.auth.presentation.data.request.SignUpRequest
+import com.dol.domain.auth.service.SignUpService
+import com.dol.domain.user.entity.User
+import com.dol.domain.user.enums.Authority
+import com.dol.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@Transactional(rollbackFor = [Exception::class])
+class SignUpServiceImpl(
+    private val userRepository: UserRepository,
+    private val securityUtil: SecurityUtil
+) : SignUpService {
+    override fun execute(signUpRequest: SignUpRequest) {
+        if (userRepository.existsById(signUpRequest.id)) throw DuplicateIdException()
+        if (userRepository.existsByNickName(signUpRequest.nickName)) throw DuplicateNickNameException()
+
+        val user = User(
+            idx = UUID.randomUUID(),
+            id = signUpRequest.id,
+            name = signUpRequest.name,
+            nickName = signUpRequest.nickName,
+            phoneNumber = signUpRequest.phoneNumber,
+            password = securityUtil.passwordEncode(signUpRequest.password),
+            city = signUpRequest.city,
+            gu = signUpRequest.gu,
+            profileUrl = signUpRequest.profileUrl,
+            authority = Authority.ROLE_USER
+        )
+        userRepository.save(user)
+    }
+}
