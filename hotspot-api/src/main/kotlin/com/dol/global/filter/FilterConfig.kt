9e36da60bd686fd4ade9c8e41d0c81e9f3a123eb
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-01 10:56:31 +0900

    update :: 클래스명 변경

--- hotspot-api/src/main/kotlin/com/dol/global/filter/FilterConfig.kt
+++ hotspot-api/src/main/kotlin/com/dol/global/filter/FilterConfig.kt
@@ -1,18 +1,18 @@
 package com.dol.global.filter
 
 import com.dol.global.security.filter.ExceptionHandlerFilter
-import com.dol.global.security.jwt.TokenParseAdapter
+import com.dol.global.security.jwt.TokenParser
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 class FilterConfig(
-    private val tokenParseAdapter: TokenParseAdapter
+    private val tokenParser: TokenParser
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
-        builder.addFilterBefore(JwtRequestFilter(tokenParseAdapter), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(JwtRequestFilter(tokenParser), UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(ExceptionHandlerFilter(), JwtRequestFilter::class.java)
     }
 
