0fafdff6d679e0794b9b6b77c1aece0fec61be54
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-01 10:57:55 +0900

    feat :: 로그인 api 구현

--- hotspot-api/src/main/kotlin/com/dol/domain/auth/presentation/AuthController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/auth/presentation/AuthController.kt
@@ -1,6 +1,10 @@
 package com.dol.domain.auth.presentation
 
+import com.dol.domain.auth.mapper.AuthMapper
+import com.dol.domain.auth.presentation.data.request.SignInRequest
 import com.dol.domain.auth.presentation.data.request.SignUpRequest
+import com.dol.domain.auth.presentation.data.response.TokenResponse
+import com.dol.domain.auth.service.SignInService
 import com.dol.domain.auth.service.SignUpService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class AuthController(
-    private val signUpService: SignUpService
+    private val authMapper: AuthMapper,
+    private val signUpService: SignUpService,
+    private val signInService: SignInService
 ) {
 
     @PostMapping("/signup")
     fun signUp(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<Void> =
         signUpService.execute(signUpRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PostMapping("/signin")
+    fun signIn(@RequestBody signInRequest: SignInRequest): ResponseEntity<TokenResponse> =
+        signInService.execute(signInRequest)
+            .let { ResponseEntity.ok(authMapper.toResponse(it)) }
 
 }
