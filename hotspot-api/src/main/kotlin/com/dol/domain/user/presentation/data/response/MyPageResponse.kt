bb127feae633addc69611dc4c78db5f2b2a8fc20
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-04 15:56:02 +0900

    add :: my-page response

---
+++ hotspot-api/src/main/kotlin/com/dol/domain/user/presentation/data/response/MyPageResponse.kt
@@ -0,0 +1,8 @@
+package com.dol.domain.user.presentation.data.response
+
+data class MyPageResponse(
+    val id: String,
+    val name: String,
+    val nickName: String,
+    val profileUrl: String?
+)
