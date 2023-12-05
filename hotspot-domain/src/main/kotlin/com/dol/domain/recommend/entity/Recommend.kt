6177fd057c23223234882c7e1585d05273ba90c7
KimTaeO <taeo0328@gmail.com>
2023-12-05 12:16:10 +0900

    add :: 추천에 createDate 필드 추가

--- hotspot-domain/src/main/kotlin/com/dol/domain/recommend/entity/Recommend.kt
+++ hotspot-domain/src/main/kotlin/com/dol/domain/recommend/entity/Recommend.kt
@@ -3,13 +3,14 @@ package com.dol.domain.recommend.entity
 import com.dol.common.entity.BaseUUIDEntity
 import com.dol.domain.hotplace.entity.Hotplace
 import com.dol.domain.user.entity.User
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 import java.util.*
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
+import javax.persistence.*
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 class Recommend(
     @Column(name = "idx", columnDefinition = "BINARY(16)", nullable = false)
     override val idx: UUID,
@@ -20,5 +21,8 @@ class Recommend(
 
     @ManyToOne
     @JoinColumn(name = "hotplace_idx", nullable = false)
-    val hotplace: Hotplace
+    val hotplace: Hotplace,
+
+    @CreatedDate
+    val createdAt: LocalDateTime
 ) : BaseUUIDEntity(idx)
