630b9f33761a571d03024e8cb22ce8c5913b540b
KimTaeO <taeo0328@gmail.com>
2023-12-05 12:14:02 +0900

    create :: 추천 entity 생성

---
+++ hotspot-domain/src/main/kotlin/com/dol/domain/recommend/entity/Recommend.kt
@@ -0,0 +1,24 @@
+package com.dol.domain.recommend.entity
+
+import com.dol.common.entity.BaseUUIDEntity
+import com.dol.domain.hotplace.entity.Hotplace
+import com.dol.domain.user.entity.User
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity
+class Recommend(
+    @Column(name = "idx", columnDefinition = "BINARY(16)", nullable = false)
+    override val idx: UUID,
+
+    @ManyToOne
+    @JoinColumn(name = "user_idx", nullable = false)
+    val user: User,
+
+    @ManyToOne
+    @JoinColumn(name = "hotplace_idx", nullable = false)
+    val hotplace: Hotplace
+) : BaseUUIDEntity(idx)
