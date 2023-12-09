19f86ffb2806824a78c53e5078390c33871a09bf
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-09 12:59:07 +0900

    feat :: 팔로우 취소 api

--- hotspot-api/src/main/kotlin/com/dol/domain/follow/presentation/FollowController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/follow/presentation/FollowController.kt
@@ -1,9 +1,12 @@
 package com.dol.domain.follow.presentation
 
+import com.dol.domain.follow.presentation.data.request.FollowCancelRequest
 import com.dol.domain.follow.presentation.data.request.FollowRequest
+import com.dol.domain.follow.service.FollowCancelService
 import com.dol.domain.follow.service.FollowService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/follow")
 class FollowController(
-    private val followService: FollowService
+    private val followService: FollowService,
+    private val followCancelService: FollowCancelService
 ) {
 
     @PostMapping
     fun follow(@RequestBody followRequest: FollowRequest) : ResponseEntity<Void> =
         followService.execute(followRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @DeleteMapping
+    fun followCancel(@RequestBody followCancelRequest: FollowCancelRequest) : ResponseEntity<Void> =
+        followCancelService.execute(followCancelRequest)
+            .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 
 }
