9bb72a8774738320dfe79d818a84db4d29fd3cf4
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-13 23:19:32 +0900

    feat :: follow recommend service

---
+++ hotspot-api/src/main/kotlin/com/dol/domain/follow/service/impl/FollowRecommendServiceImpl.kt
@@ -0,0 +1,25 @@
+package com.dol.domain.follow.service.impl
+
+import com.dol.domain.follow.presentation.data.response.FollowRecommendResponse
+import com.dol.domain.follow.service.FollowRecommendService
+import com.dol.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import kotlin.random.Random
+
+@Service
+@Transactional(rollbackFor = [Exception::class])
+class FollowRecommendServiceImpl(
+    private val userRepository: UserRepository
+) : FollowRecommendService {
+    override fun execute(): List<FollowRecommendResponse> {
+        val allUser = userRepository.findAll()
+        return allUser.shuffled(Random).map {
+            FollowRecommendResponse(
+                id = it.id,
+                name = it.name,
+                profileUrl = it.profileUrl
+            )
+        }
+    }
+}
