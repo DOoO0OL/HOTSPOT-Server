ef0f9499b8d399aec157f71bf48571ec079bbac7
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-17 02:02:19 +0900

    add :: s3 properties

--- hotspot-api/src/main/kotlin/com/dol/global/config/PropertiesScanConfig.kt
+++ hotspot-api/src/main/kotlin/com/dol/global/config/PropertiesScanConfig.kt
@@ -3,6 +3,8 @@ package com.dol.global.config
 import com.dol.global.security.jwt.common.properties.JwtExpTimeProperties
 import com.dol.global.security.jwt.common.properties.JwtProperties
 import com.dol.thirdparty.coolSms.properties.CoolSmsProperties
+import com.dol.thirdparty.aws.properties.AwsProperties
+import com.dol.thirdparty.aws.properties.s3.AwsS3Properties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
@@ -11,7 +13,9 @@ import org.springframework.context.annotation.Configuration
     basePackageClasses = [
         JwtProperties::class,
         JwtExpTimeProperties::class,
-        CoolSmsProperties::class
+        CoolSmsProperties::class,
+        AwsProperties::class,
+        AwsS3Properties::class
     ]
 )
 class PropertiesScanConfig
