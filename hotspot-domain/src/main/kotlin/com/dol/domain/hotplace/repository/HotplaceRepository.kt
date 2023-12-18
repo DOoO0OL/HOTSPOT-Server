f2143b3fccbb303fdc59e14fba7891d689dd0c3d
KimTaeO <taeo0328@gmail.com>
2023-12-18 21:01:11 +0900

    update :: imageURL 프로퍼티에 nullable 옵션 허용

--- hotspot-domain/src/main/kotlin/com/dol/domain/hotplace/repository/HotplaceRepository.kt
+++ hotspot-domain/src/main/kotlin/com/dol/domain/hotplace/repository/HotplaceRepository.kt
@@ -2,6 +2,7 @@ package com.dol.domain.hotplace.repository
 
 import com.dol.domain.hotplace.entity.Hotplace
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
-interface HotplaceRepository : CrudRepository<Hotplace, Long> {
+interface HotplaceRepository : CrudRepository<Hotplace, UUID> {
 }
