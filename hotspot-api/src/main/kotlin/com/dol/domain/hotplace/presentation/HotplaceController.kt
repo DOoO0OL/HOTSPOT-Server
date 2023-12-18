4d9c02df7ef853025811b4eac2e42771d402243f
KimTaeO <taeo0328@gmail.com>
2023-12-18 16:16:48 +0900

    fix :: controller uri 오탈자 수정

--- hotspot-api/src/main/kotlin/com/dol/domain/hotplace/presentation/HotplaceController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/hotplace/presentation/HotplaceController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("hotplace")
+@RequestMapping("/hotplace")
 class HotplaceController(
     private val createHotplaceService: CreateHotplaceService
 ) {
