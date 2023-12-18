fca3288cdbc5b5cc4f88855f8825702e3aacc5e8
KimTaeO <taeo0328@gmail.com>
2023-12-18 21:01:38 +0900

    create :: query hotplace response

---
+++ hotspot-api/src/main/kotlin/com/dol/domain/hotplace/presentation/data/response/QueryHotplaceResponse.kt
@@ -0,0 +1,12 @@
+package com.dol.domain.hotplace.presentation.data.response
+
+import java.util.UUID
+
+data class QueryHotplaceResponse(
+    val idx: UUID,
+    val name: String,
+    val address: String,
+    val instagramId: String,
+    val recommend: Long,
+    val imgURL: String?
+)
