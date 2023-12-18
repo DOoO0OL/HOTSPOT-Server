dae473534fe33ce6e865339e5b672585b0c8b982
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-18 19:37:22 +0900

    update :: request part

--- hotspot-api/src/main/kotlin/com/dol/domain/image/presentation/ImageController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/image/presentation/ImageController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
@@ -15,7 +16,7 @@ class ImageController(
     private val imageUploadService: ImageUploadService
 ) {
     @PostMapping
-    fun uploadImage(@RequestParam("image") file: MultipartFile) : ResponseEntity<ImageUrlResponse> =
+    fun uploadImage(@RequestPart("image") file: MultipartFile) : ResponseEntity<ImageUrlResponse> =
         imageUploadService.execute(file)
             .let { ResponseEntity.ok(it) }
 
