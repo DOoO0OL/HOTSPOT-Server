96e7d87be6238499be35bf5b283a574c4fe69c04
KimTaeO <taeo0328@gmail.com>
2023-12-18 21:15:20 +0900

    add :: query hotplace controller

--- hotspot-api/src/main/kotlin/com/dol/domain/hotplace/presentation/HotplaceController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/hotplace/presentation/HotplaceController.kt
@@ -1,22 +1,29 @@
 package com.dol.domain.hotplace.presentation
 
 import com.dol.domain.hotplace.presentation.data.request.CreateHotplaceRequest
+import com.dol.domain.hotplace.presentation.data.response.QueryHotplaceResponse
 import com.dol.domain.hotplace.service.CreateHotplaceService
+import com.dol.domain.hotplace.service.QueryHotplaceService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
 
 @RestController
 @RequestMapping("/hotplace")
 class HotplaceController(
-    private val createHotplaceService: CreateHotplaceService
+    private val createHotplaceService: CreateHotplaceService,
+    private val queryHotplaceService: QueryHotplaceService
 ) {
     @PostMapping
     fun createHotplace(@RequestBody createHotplaceRequest: CreateHotplaceRequest): ResponseEntity<Void> {
         createHotplaceService.execute(createHotplaceRequest)
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping("/{idx}")
+    fun queryHotplace(@PathVariable idx: UUID): ResponseEntity<QueryHotplaceResponse> {
+        val response = queryHotplaceService.execute(idx)
+        return ResponseEntity.ok(response)
     }
 }
