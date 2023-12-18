3287d6d3d146965fb13c3514da22f08d8dcb9412
KimGyeongsuuu <sk62628504@gmail.com>
2023-12-18 09:32:08 +0900

    feat :: update profile image api

--- hotspot-api/src/main/kotlin/com/dol/domain/user/presentation/UserController.kt
+++ hotspot-api/src/main/kotlin/com/dol/domain/user/presentation/UserController.kt
@@ -1,20 +1,31 @@
 package com.dol.domain.user.presentation
 
+import com.dol.domain.image.presentation.data.response.ImageUrlResponse
 import com.dol.domain.user.presentation.data.response.MyPageResponse
 import com.dol.domain.user.service.MyPageService
+import com.dol.domain.user.service.UploadProfileImageService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val myPageService: MyPageService
+    private val myPageService: MyPageService,
+    private val uploadProfileImageService: UploadProfileImageService
 ) {
     @GetMapping("/my-page")
     fun myPage(): ResponseEntity<MyPageResponse> =
         myPageService.execute()
+            .let { ResponseEntity.ok(it) }
+
+    @PostMapping("profileImage")
+    fun uploadProfileImage(@RequestParam("image") file: MultipartFile) : ResponseEntity<ImageUrlResponse> =
+        uploadProfileImageService.execute(file)
             .let { ResponseEntity.ok(it) }
 
 }
