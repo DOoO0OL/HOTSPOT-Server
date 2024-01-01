037016bcab3357a9a1adf112f7fd63046c4fbce4
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-02 00:37:40 +0900

    update :: jar file 생성

--- hotspot-api/build.gradle.kts
+++ hotspot-api/build.gradle.kts
@@ -19,3 +19,7 @@ dependencies {
     implementation("com.querydsl:querydsl-jpa:5.0.0")
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 }
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
